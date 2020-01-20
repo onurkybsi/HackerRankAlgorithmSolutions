e04b90030114e0ed2dc37d6d0ccf73cb1bb9dec1
onurkybsi <onurbpm@outlook.com>
2020-01-20 13:39:18 +0300

    Save14

--- CircularArrayRotation.c
+++ CircularArrayRotation.c
@@ -5,40 +5,25 @@
 
 int* circularArrayRotation(int a_count, int* a, int k, int queries_count, int* queries, int* result_count) {
 
+    int* result = (int*)malloc(sizeof(int)*queries_count);
+
     for (int i = 0; i < queries_count; i++)
     {
-        result_count[i] = a[(queries[i]+(a_count%k))%queries_count];
+        if(k > a_count)
+            result[i] = a[(queries[i]+(k*a_count)-k)%a_count];
+        else
+            result[i] = a[(queries[i]+a_count-k)%a_count];
     }
     
-    return result_count;
+    *result_count = queries_count;
+
+    return result;
 }
 
 
 int main(){
 
-    int* a = (int*)malloc(3*sizeof(int));
 
-    a[0] = 1;
-    a[1] = 2;
-    a[2] = 3;
-
-    int querycount = 3;
-
-    int* q = (int*)malloc(querycount*sizeof(int));
-
-    q[0] = 0;
-    q[1] = 1;
-    q[2] = 2;
-    
-
-    int* result = (int*)malloc(querycount*sizeof(int));
-
-    for (int i = 0; i < querycount; i++)
-    {
-        printf("%d ",circularArrayRotation(3,a,2,querycount,q,result)[i]);
-    }
-    
- 
     system("PAUSE");
 
     return 0;
---
+++ SequenceEquation.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int* permutationEquation(int p_count, int* p, int* result_count) {
+
+    int* result = (int*)malloc(sizeof(int)*p_count);
+    
+    
+
+    *result_count = p_count;
+
+    return result;
+}
+
+int main(){
+
+   
+
+    system("PAUSE");
+
+    return 0;
+
+}
