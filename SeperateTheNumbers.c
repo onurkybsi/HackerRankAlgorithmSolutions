6caf9cd451186cb9c1eb3b45462528f9684510e2
onurkybsi <onurbpm@outlook.com>
2020-02-25 12:43:59 +0300

    Save35

---
+++ SeperateTheNumbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void separateNumbers(char* s) {
+
+    
+
+
+}
+
+int main(){
+
+    system("PAUSE");
+
+    return 0;
+
+}
